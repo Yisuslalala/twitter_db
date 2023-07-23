7a606de51c336209e58d4c2018c6bd5a9950728a
Jesús Quiñones <1930318@upv.edu.mx>
2023-07-23 01:15:00 -0600

    Upload final details

--- sql_schema.sql
+++ sql_schema.sql
@@ -2,15 +2,18 @@ DROP DATABASE IF EXISTS twitter_db;
 
 CREATE DATABASE twitter_db;
 
+USE twitter_db;
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users ( 
     user_id INT NOT NULL AUTO_INCREMENT, 
     user_handle VARCHAR(50) NOT NULL UNIQUE, 
-    email_adress VARCHAR(50) NOT NULL UNIQUE, 
+    email_address VARCHAR(50) NOT NULL UNIQUE, 
     first_name VARCHAR(100) NOT NULL, 
     last_name VARCHAR(100) NOT NULL, 
     phonenumber CHAR(10) UNIQUE, 
+    follower_count INT NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT (NOW()), 
     PRIMARY KEY(user_id)
 );
@@ -22,7 +25,7 @@ VALUES
     ("AuroraOficial", "aurora_singer@vevo.com", "Aurora", "Ødegaard", 1000000001),
     ("Pepito", "pepito_arqui@hotmail.com", "José", "Ramírez", 8341234567);
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS followers;
 
 CREATE TABLE followers (
     follower_id INT NOT NULL,
@@ -97,9 +100,44 @@ WHERE user_id IN (
 /*
 DELETE FROM tweets WHERE tweet_id = 1;
 */
-
+-- Update likes
 UPDATE tweets SET num_likes = num_likes + 1 WHERE tweet_id = 2;
--- Replace text
 
+-- Replace text
 UPDATE tweets SET tweet_text = REPLACE(tweet_text, "was", "am")
 WHERE tweet_text LIKE '%was%';
+
+
+CREATE TABLE tweet_likes(
+    user_id INT NOT NULL,
+    tweet_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (tweet_id) REFERENCES tweets(tweet_id),
+    PRIMARY KEY (user_id, tweet_id)
+);
+
+INSERT INTO tweet_likes (user_id, tweet_id)
+VALUES
+    (1, 3),
+    (1, 4),
+    (1, 2),
+    (3, 3),
+    (2, 4),
+    (4, 4);
+
+-- Obtener el número de likes para cada tuit
+SELECT tweet_id, COUNT(*) AS likes
+FROM tweet_likes
+GROUP BY tweet_id;
+
+/*CREATE TRIGGER*/
+DELIMITER $$
+CREATE TRIGGER increase_follower_count
+    AFTER INSERT ON followers
+    FOR EACH ROW
+    BEGIN 
+        UPDATE users SET follower_count = follower_count + 1
+        WHERE user_id = NEW.following_id;
+    END $$
+DELIMITER ;
+
